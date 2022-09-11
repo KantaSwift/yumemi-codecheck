71f7bc0d8f6bf482d3fb40bdba2ace2129cbeab3
KantaSwift <github@kanta-_-soccer.docomo.ne.jp>
2022-09-12 02:03:37 +0900

    不必要なコメントの削除とインデント、スペースを整える

--- iOSEngineerCodeCheck/ResultViewController.swift
+++ iOSEngineerCodeCheck/ResultViewController.swift
@@ -11,18 +11,15 @@ import UIKit
 class ResultViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var languageLabel: UILabel!
-    
     @IBOutlet weak var starsLabel: UILabel!
     @IBOutlet weak var watchersLabel: UILabel!
     @IBOutlet weak var forksLabel: UILabel!
     @IBOutlet weak var issuesLabel: UILabel!
     
     var searchViewController: SearchViewController!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,28 +30,28 @@ class ResultViewController: UIViewController {
         watchersLabel.text = "\(repository.watchersCount) watchers"
         forksLabel.text = "\(repository.forksCount) forks"
         issuesLabel.text = "\(repository.openIssuesCount) open issues"
+        
         getImage()
         
     }
     
-    func getImage(){
+    func getImage () {
         
         let repository = searchViewController.items[searchViewController.index]
         titleLabel.text = repository.fullName
-//        let owner = repository.fullName
         let imageURL = repository.owner.avatarUrl
         guard  let url = URL(string: imageURL) else {
             print("error")
             return
         }
-          
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, _) in
-    
+            
             guard let data = data else {
                 print("data is nil")
                 return
             }
+            
             let image = UIImage(data: data)
             
             DispatchQueue.main.async {
--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SearchViewController: UITableViewController, UISearchBarDelegate {
-
+    
     @IBOutlet weak var SearchBar: UISearchBar!
     
     var items = [Repository.Item]()
@@ -20,15 +20,10 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        SearchBar.text = "GitHubのリポジトリを検索できるよー"
+        
+        SearchBar.placeholder = "Githubリポジトリの検索"
         SearchBar.delegate = self
-    }
-    
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        // ↓こうすれば初期のテキストを消せる
-        searchBar.text = ""
-        return true
+        
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -74,7 +69,7 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
         task?.resume()
         
     }
-        
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -94,14 +89,14 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
         let cell = UITableViewCell()
         let repository = items[indexPath.row]
         cell.textLabel?.text = repository.fullName
-        cell.detailTextLabel?.text = repository.language 
+        cell.detailTextLabel?.text = repository.language
         cell.tag = indexPath.row
         return cell
         
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 画面遷移時に呼ばれる
+    
         index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
         
